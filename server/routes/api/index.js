8f49f128846bb53b5070f8730a8d98c1c3980ca6
Tamnhu <tamnhuu.213@gmail.com>
2025-10-11 13:09:23 +0700

    Phase 2: Set up backend + fronend with basic about user and product

---
+++ server/routes/api/index.js
@@ -0,0 +1,34 @@
+const router = require('express').Router();
+
+const authRoutes = require('./auth');
+const userRoutes = require('./user');
+const addressRoutes = require('./address');
+const newsletterRoutes = require('./newsletter');
+const productRoutes = require('./product');
+const categoryRoutes = require('./category');
+const brandRoutes = require('./brand');
+const contactRoutes = require('./contact');
+const merchantRoutes = require('./merchant');
+const cartRoutes = require('./cart');
+const orderRoutes = require('./order');
+const reviewRoutes = require('./review');
+const wishlistRoutes = require('./wishlist');
+
+// auth routes
+router.use('/auth', authRoutes);
+
+// user routes
+router.use('/user', userRoutes);
+
+// address routes
+router.use('/address', addressRoutes);
+
+// newsletter routes
+router.use('/newsletter', newsletterRoutes);
+
+// product routes
+router.use('/product', productRoutes);
+
+
+
+module.exports = router;
